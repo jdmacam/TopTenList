2fa0a1d369a2a795af95c00ab0f987fa4e47f797
Joseph Macam <jdmacam@gmail.com>
2020-04-03 22:16:36 -0700

    Assignment x12

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -1,14 +1,42 @@
 //Manages a list of top ten hyperlinks
 #include <string>
+#include <iostream>
 #include "TopTenList.h"
 #include "Hyperlink.h"
 
 using std::string;
+using std::cout;
+using std::endl;
+
+TopTenList::TopTenList()
+{
+  _list.resize(10);
+}
 
 TopTenList::TopTenList(string title)
 {
   _title = title;
   _list.resize(10);
+}
+
+void TopTenList::display_forward()
+{
+  int i;
+  for(i = 0; i < _list.size(); i++)
+  {
+    cout << i+1 << ". ";
+    cout << _list[i].text << ", " << _list[i].url << endl;
+  }
+}
+
+void TopTenList::display_backward()
+{
+  int i;
+  for(i = _list.size()-1; i >= 0; i--)
+  {
+    cout << i+1 << ". ";
+    cout << _list[i].text << ", " << _list[i].url << endl;
+  }
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
